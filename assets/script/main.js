c1d89ebe118d43ac344f7c699e5265f95cb94aa5
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2021-09-20 16:29:40 -0500

    Added the ui game

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,58 +1,81 @@
 "use strict"
 
-function computerPlay(){
+const computerSelection = () => {
     const plays = ["rock", "paper", "scissors"]
     let play = Math.floor(Math.random() * 3)
     return plays[play]
 }
 
-function playerPlay(){
-    let play =  prompt("Insert your play").toLowerCase()
-    return play
+const playerSelection = (event) => {
+    player = event.target.id;
+
+    if(player === "rock" || player === "paper" || player === "scissors")
+    {
+        console.log("player: ", player)
+        const winner = playRound(computerSelection(), player)
+        if(winner === "player"){
+            playerCount++
+            round ++
+            player = ""
+        } else if(winner === "computer"){
+            computerCount++
+            round ++
+            player = ""
+        } else{
+            round ++
+            player = ""
+        }
+        console.log(round, winner, playerCount, computerCount)
+        
+        if(round === 5){
+            if(playerCount > computerCount){
+                GameWinner = "Player wins";
+            } else if(computerCount > playerCount){
+                GameWinner = "Computer wins";
+            } else{
+                GameWinner = "It's a draw";
+            }
+            text.innerText = GameWinner;
+            roundResults.appendChild(text)
+            round = 0;
+            computerCount = 0;
+            playerCount= 0   
+        }
+    }
 }
 
 function playRound(computer, player){
-    console.log(`computer: ${computer} --- player: ${player}`)
-    if(computer === "rock"){
-        if(player === "rock"){
-            console.log("draw")
-        } else if(player === "paper"){
-            console.log("player wins")
-        } else if(player === "scissors"){
-            console.log("computer wins")
-        } else{
-            console.log("Oops, you entered the wrong selection")
-        }
-    } else if(computer === "paper"){
-        if(player === "rock"){
-            console.log("computer wins")
-        } else if(player === "paper"){
-            console.log("draw")
-        } else if(player === "scissors"){
-            console.log(" player wins")
-        } else{
-            console.log("Oops, you entered the wrong selection")
-        }
-    } else if(computer === "scissors"){
-        if(player === "rock"){
-            console.log("player wins")
-        } else if(player === "paper"){
-            console.log("computer wins")
-        } else if(player === "scissors"){
-            console.log("draw")
-        } else{
-            console.log("Oops, you entered the wrong selection")
-        }
+    let winner = ""
+        
+    switch(player){
+        case "rock":
+            (computer === "scissors")? winner = "player": 
+            (computer === "paper")? winner = "computer": winner = "draw";
+            return winner;
+        case "paper":
+            (computer === "rock")? winner = "player": 
+            (computer === "scissors")? winner = "computer": winner = "draw";
+            return winner;
+        case "scissors":
+            (computer === "paper")? winner = "player": 
+            (computer === "rock")? winner = "computer": winner = "draw";
+            return winner;
     }
 }
 
-function game(){
-    for(let i = 0; i<5; i++){
-        const computerSelection = computerPlay()
-        const playerSelection = playerPlay()
-        playRound(computerSelection, playerSelection)
-    }
-}
+const paper = document.querySelector("#paper")
+const rock = document.querySelector("#rock")
+const scissors = document.querySelector("#scissors")
+const roundResults = document.querySelector(".winner")
 
-game()
+paper.addEventListener("click", playerSelection)
+rock.addEventListener("click", playerSelection)
+scissors.addEventListener("click", playerSelection)
 
+let playerCount = 0
+let computerCount = 0
+let round = 0
+let GameWinner = ""    
+let text = document.createElement("h2")
+
+let player = ""
